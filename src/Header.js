dbb4d93291c4ed9deb0c1be99a26592486cb33cb
professorRob <professorRob78@yahoo.com>
2021-10-07 08:44:55 -0400

    Its not Ashley, its Laniece

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function header() {
     return (
       <div className="header">
-          <h1>NeShaunda A. Edmonds</h1>
+          <h1>NeShaunda L. Edmonds</h1>
           <h3>Orchid Legal Services</h3>
       </div>
     );
