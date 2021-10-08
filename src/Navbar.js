fd572fe7c42bbaef24d9a564cf40607ed9fe8420
professorRob <professorRob78@yahoo.com>
2021-10-08 15:38:19 -0400

    Let's see if she likes pink

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Navbar.css';
 function Navbar() {
     return (
-        <div data-theme="theme-blueberry">
+        <div data-theme="theme-briyah">
             <ul>
                 <li>HOME</li>
                 <li>SERVICES</li>
